adf1d98ea64f3fd933a41c691374a6c4e4ac648a
cludwell <cludwell@gmail.com>
2023-07-26 00:17:23 -0700

    working on auth funcs

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,11 +1,11 @@
 import { hash, compare } from 'bcryptjs';
 
-export async function hashPassword(password: string) {
+export async function hashPassword(password: string): Promise<string> {
   const hashedPassword = await hash(password, 12);
   return hashedPassword;
 }
 
-export async function verifyPassword(password: string, hashedPassword: string) {
+export async function verifyPassword(password: string, hashedPassword: string): Promise<boolean> {
   const isValid = await compare(password, hashedPassword);
   return isValid;
 }
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,42 +1,38 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-
-import NextAuth from 'next-auth/next'
-import Providers from 'next-auth/providers';
+import { PrismaClient, users as PrismaUser } from '@prisma/client';
+const prisma = new PrismaClient();
+import NextAuth from 'next-auth';
+import CredentialsProvider from "next-auth/providers/credentials"
+import { verifyPassword } from '../../../../lib/auth';
 
 export default NextAuth({
-    session: {
-        jwt: true,
-    },
-    providers: [
-      // Add your authentication providers here
-      Providers.Credentials({
-        // The name to display on the sign-in form (e.g., 'Email')
-
-
-        name: 'Credentials',
-        credentials: {
-            email: { label: 'Email', type: 'text' },
-            password: { label: 'Password', type: 'password' }
-        },
-        authorize: async (credentials: string) => {
-          // Add your custom login logic here
-          // For example, use Prisma to validate the user's credentials
-          const user = await prisma.users.findUnique({
-            where: { email: credentials.email }
+  session: {
+    strategy: 'jwt'
+  },
+  providers: [
+    // Add your authentication providers here
+    CredentialsProvider({
+      // The name to display on the sign-in form (e.g., 'Email')
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      authorize: async (credentials, req): Promise<any> => {
+        if (!credentials || !credentials.email || !credentials.password) throw new Error('Invalid Credentials')
+        const user = await prisma.users.findUnique({
+          where: { email: credentials.email },
         });
-          if (user && user.password === credentials.password) {
-            return Promise.resolve(user);
-          } else {
-            return Promise.resolve(null);
-          }
-        }
-      }),
-      // Add other providers (e.g., Google, GitHub) as needed
-      // Providers.Google({
-      //   clientId: process.env.GOOGLE_CLIENT_ID,
-      //   clientSecret: process.env.GOOGLE_CLIENT_SECRET
-      // }),
-    ],
-    // Add other configurations as needed
-  });
+        if (!user) throw new Error('No user found!')
+        const isValid = await verifyPassword(credentials.password, user.hashed_password)
+        if (!isValid) throw new Error('Password doesnt match!')
+        else return user
+      },
+    }),
+    // Add other providers (e.g., Google, GitHub) as needed
+    // Providers.Google({
+    //   clientId: process.env.GOOGLE_CLIENT_ID,
+    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    // }),
+  ],
+  // Add other configurations as needed
+});
