7861e4be6640899c9a8bff416a94b94c4113ad3b
cludwell <cludwell@gmail.com>
2023-08-28 23:56:13 -0700

    formatting

--- src/pages/characters.tsx
+++ src/pages/characters.tsx
@@ -7,72 +7,82 @@ import { useSelector, useDispatch } from "react-redux";
 
 export default function CreateCharacter() {
   const { data: session, status: loading } = useSession();
-  const [ hasLoaded, setHasLoaded ] = useState(false)
+  const [hasLoaded, setHasLoaded] = useState(false);
   const dispatch = useDispatch<AppDispatch>();
+  
   useEffect(() => {
     const loadCharacters = async () => {
       dispatch(allCharactersRequest());
-      setHasLoaded(true)
+      setHasLoaded(true);
     };
     loadCharacters();
   }, [dispatch]);
 
   const characters = useSelector(allCharactersState);
 
-  if (!hasLoaded || !characters) return 'Loading'
+  if (!hasLoaded || !characters) return "Loading";
 
   return (
     <main className={`flex min-h-screen flex-col items-center p-24`}>
       {/* title */}
       <h1 className="text-3xl federant font-bold">Character Creation Method</h1>
+      <h2>Choose how you would like to create your character</h2>
       {/* divider */}
       <div className="flex flex-col w-full">
         <div className="divider"></div>
       </div>
       {/* card container */}
-      <div className="flex flew-row justify-around" >
-
-      {/* card to display character selections */}
-      <div className="card card-compact w-96 bg-base-100 shadow-xl">
-        <figure >
-          <img src={characters && characters[0]?.characters.imgUrl} alt="character preview"/>
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">STANDARD</h2>
-          <p>Create a character using a step-by-step approach</p>
-          <div className="card-actions justify-end"></div>
+      <div className="flex flex-row flex-wrap justify-evenly">
+        {/* card to display character selections */}
+        <div className="card card-compact w-96 bg-base-100 shadow-xl m-4">
+          <figure>
+            <img
+              src={characters && characters[0]?.characters.imgUrl}
+              alt="character preview"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">STANDARD</h2>
+            <p>Create a character using a step-by-step approach</p>
+            <div className="card-actions justify-end"></div>
+          </div>
+          <button className="btn btn-primary rounded-b-2xl rounded-t-none flex justify-end">
+            START BUILDING
+            <svg width="16" height="16" viewBox="0 0 24 24" fill="none">
+              <path
+                d="M8 3L16 12L8 21"
+                stroke="currentColor"
+                strokeWidth="2"
+              ></path>
+            </svg>
+          </button>
         </div>
-        <button className="btn btn-primary rounded-b-2xl rounded-t-none flex justify-end">
-          START BUILDING
-          <svg width="16" height="16" viewBox="0 0 24 24" fill="none">
-            <path
-              d="M8 3L16 12L8 21"
-              stroke="currentColor"
-              strokeWidth="2"
-            ></path>
-          </svg>
-        </button>
-      </div>
-      <div className="card card-compact w-96 bg-base-100 shadow-xl">
-        <figure >
-          <img src={characters && characters[2]?.characters.imgUrl} alt="character preview"/>
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">PREMADE</h2>
-          <p>Browse a selection of ready-to-play, premade characters and claim one to your account.</p>
-          <div className="card-actions justify-end"></div>
+        <div className="card card-compact w-96 bg-base-100 shadow-xl m-4">
+          <figure>
+            <img
+              src={characters && characters[2]?.characters.imgUrl}
+              alt="character preview"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">PREMADE</h2>
+            <p>
+              Browse a selection of ready-to-play, premade characters and claim
+              one to your account.
+            </p>
+            <div className="card-actions justify-end"></div>
+          </div>
+          <button className="btn btn-primary rounded-b-2xl rounded-t-none flex justify-end">
+            START BROWSING
+            <svg width="16" height="16" viewBox="0 0 24 24" fill="none">
+              <path
+                d="M8 3L16 12L8 21"
+                stroke="currentColor"
+                strokeWidth="2"
+              ></path>
+            </svg>
+          </button>
         </div>
-        <button className="btn btn-primary rounded-b-2xl rounded-t-none flex justify-end">
-          START BROWSING
-          <svg width="16" height="16" viewBox="0 0 24 24" fill="none">
-            <path
-              d="M8 3L16 12L8 21"
-              stroke="currentColor"
-              strokeWidth="2"
-            ></path>
-          </svg>
-        </button>
-      </div>
       </div>
     </main>
   );
