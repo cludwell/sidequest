72b605c2b2113ac9bf7c0e7c315ecf3b6e4d956c
cludwell <cludwell@gmail.com>
2023-10-23 13:17:43 -0700

    python backend deleted

--- backend/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .db import db
-from .user import User
-from .character import Character
-from .scenario import Scenario
-from .user_scenario import UserScenario
-from .db import environment, SCHEMA
